048a83c7d399bc95940de06f01b36ed7827b6b18
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-06-12 18:42:34 +0100

    Added color utils

--- Rosary/utils/ColorUtils.swift
+++ Rosary/utils/ColorUtils.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUICore
+
+extension Color {
+    static var random: Color {
+        Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
+
+let colors: [Color] = [.primary, .secondary, Color(red: 251/255, green: 128/255, blue: 128/255)]
+
+var randomColor: Color {
+    colors.randomElement() ?? .blue
+}
